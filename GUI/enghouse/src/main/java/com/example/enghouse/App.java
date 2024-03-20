012f72f5062ed949c46728871a5de38a2df1993d
NguyenMinhQuan <sleepifoxx@gmail.com>
2024-03-20 23:25:06 +0700

    Add icon window

--- GUI/enghouse/src/main/java/com/example/enghouse/App.java
+++ GUI/enghouse/src/main/java/com/example/enghouse/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
 
 import java.io.IOException;
 
@@ -13,6 +14,7 @@ public class App extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("Menu.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 960, 640);
         stage.setTitle("EngHouse - Your English Learning Assistant");
+        stage.getIcons().add(new Image(App.class.getResource("icons/dictionary.png").toExternalForm()));
         stage.setScene(scene);
         stage.show();
 
